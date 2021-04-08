1b3aecb5520d73dc61289ad4668b79d8c6d6f96a
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2021-04-08 15:07:47 +0300

    Update generate_list.py

--- generate_list.py
+++ generate_list.py
@@ -16,15 +16,6 @@ while True:
     ALL_ANIME.extend(list(re.findall(regex, r)))
     page += 1
 
-page = 1
-while True:
-    r = requests.get(f"https://gogoanime.vc/anime-movies.html?page={page}").text
-    soup = BeautifulSoup(r, 'html.parser')
-    soup = soup.select('ul.listing > li')
-    if not soup:
-        break
-    ALL_ANIME.extend(list(re.findall(regex, r)))
-    page += 1
 
 with open('all_anime.json', 'w') as f:
     json.dump(ALL_ANIME, f, indent=4)
