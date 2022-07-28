a910a979eececec812f6f9babb1449491cc57e38
justfuckingaround <53124886+ArjixWasTaken@users.noreply.github.com>
2022-07-28 15:09:25 -0700

    Update GogoRandom.js

--- GogoRandom.js
+++ GogoRandom.js
@@ -1,25 +1,14 @@
 // ==UserScript==
 // @name         GogoRandom
-// @namespace    http://tampermonkey.net/
-// @version      0.3
+// @namespace    https://github.com/ArjixWasTaken/my-userscripts
+// @version      0.4
 // @description  A userscript that adds a random button to gogoanime, that one feature we all wanted years now but didn't get.
 // @author       Arjix
-// @match        *://*.gogoanime.vc/*
-// @match        *://*.gogoanime.tv/*
-// @match        *://*.gogoanime.io/*
-// @match        *://*.gogoanime.in/*
-// @match        *://*.gogoanime.se/*
-// @match        *://*.gogoanime.sh/*
-// @match        *://*.gogoanime.video/*
-// @match        *://*.gogoanime.movie/*
-// @match        *://*.gogoanime.so/*
-// @match        *://*.gogoanimes.co/*
-// @match        *://*.animego.to/*
-// @match        *://*.gogoanime.ai/*
+// @license      MIT
+// @include      /.+:\/\/.*?gogoanime\..+/
 // @grant        GM_xmlhttpRequest
 // @require      https://code.jquery.com/jquery-3.5.1.min.js
 // ==/UserScript==
-
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -42,7 +31,6 @@ function getRandomInt(min, max) {
                 const index = getRandomInt(1, ALL_ANIME.length-1)
                 document.location.href = getDomainLink() + ALL_ANIME[index]
             }
-
 
     })}
     const button = `<li class="movies"><a title="Random Anime" href="#" class="random ads-evt">Random</a></li>`
